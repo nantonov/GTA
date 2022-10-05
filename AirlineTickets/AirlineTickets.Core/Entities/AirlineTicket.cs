1e14f67ad5d9beef85977357a0f12144bceab729
глеб Берёзкин <gleb.berezkin.2017@gmail.com>
2022-10-05 14:00:49 +0300

    Implement airlineticket entity and configuration

--- AirlineTickets/AirlineTickets.Core/Entities/AirlineTicket.cs
+++ AirlineTickets/AirlineTickets.Core/Entities/AirlineTicket.cs
@@ -2,6 +2,12 @@
 {
     public class AirlineTicket
     {
-
+        public int Id { get; set; }
+        public string? DeparturePlace { get; set; }
+        public string? ArrivalPlace { get; set; }
+        public DateTimeOffset DepartureTime { get; set; }
+        public DateTimeOffset ArrivalTime { get; set; }
+        public int Price { get; set; }
+        public string? PassengerCredentials { get; set; }
     }
 }
--- AirlineTickets/AirlineTickets.Data/EntityConfigurations/AirlineTicketConfiguration.cs
+++ AirlineTickets/AirlineTickets.Data/EntityConfigurations/AirlineTicketConfiguration.cs
@@ -1,6 +1,17 @@
-﻿namespace AirlineTickets.Data.EntityConfigurations
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using AirlineTickets.Core.Entities;
+
+namespace AirlineTickets.Data.EntityConfigurations
 {
-    public class AirlineTicketConfiguration
+    public class AirlineTicketConfiguration : IEntityTypeConfiguration<AirlineTicket>
     {
+        public void Configure(EntityTypeBuilder<AirlineTicket> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.PassengerCredentials).HasMaxLength(200);
+            builder.Property(t => t.DeparturePlace).HasMaxLength(200);
+            builder.Property(t => t.ArrivalPlace).HasMaxLength(200);
+        }
     }
 }
