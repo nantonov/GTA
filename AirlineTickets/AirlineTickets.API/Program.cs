1831ec5866771835acb4dddc946148642a6a752d
глеб Берёзкин <gleb.berezkin.2017@gmail.com>
2022-10-06 20:52:36 +0300

    Add DI

--- AirlineTickets/AirlineTickets.API/Program.cs
+++ AirlineTickets/AirlineTickets.API/Program.cs
@@ -1,3 +1,13 @@
+using AirlineTickets.Business.Interfaces;
+using AirlineTickets.Business.Services;
+using AirlineTickets.Core.Entities;
+using AirlineTickets.Data.Context;
+using AirlineTickets.Data.Interfaces;
+using AirlineTickets.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var options = new DbContextOptionsBuilder().Options;
+builder.Services.AddDbContext<ApplicationDbContext>(_ => new ApplicationDbContext(options: options, configurationBuilder))
+    .AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>))
+    .AddTransient<IGenericService<AirlineTicket>, AirlineTicketService>();
 
 var app = builder.Build();
 
