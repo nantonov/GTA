87311dfbcafc0258ef98762726c2441c5ab47d0e
Gleb <gleb.berezkin.2017@gmail.com>
2022-10-10 17:12:09 +0300

    Add business models

---
+++ AirlineTickets/AirlineTickets.Business/Models/City.cs
@@ -0,0 +1,13 @@
+﻿namespace AirlineTickets.Business.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Population { get; set; }
+        public int Area { get; set; }
+
+        public ICollection<AirlineTicketCity>? AirlineTicketCities { get; set; }
+        public ICollection<Hotel>? Hotels { get; set; }
+    }
+}
