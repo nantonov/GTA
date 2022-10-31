156e858b67f0f0e6e15d889d4a9fae090c1ed7a6
Gleb <gleb.berezkin.2017@gmail.com>
2022-10-31 23:01:53 +0300

    Add jwt controller

---
+++ IdentityServer/IdentityServer/Quickstart/Account/JwtController.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace IdentityServer.Quickstart.Account
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtController(IConfiguration configuration, UserManager<ApplicationUser> userManager)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("AirlineTicketsJwt")]
+        [HttpPost]
+        [Authorize]
+        public ActionResult GetAirlineTicketsJwtAsync()
+        {
+            var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Site"],
+                audience: _configuration["Jwt:Site"],
+                expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return Ok(
+                new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+        }
+    }
+}
