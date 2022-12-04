053c0d7a908ea25204b49b84e0560b18e4c94b44
Gleb <gleb.berezkin.2017@gmail.com>
2022-12-04 20:06:14 +0300

    Add exception middleware

---
+++ AirlineTicketsNotifications/AirlineTicketsNotifications.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using AirlineTicketsNotifications.API.Middleware.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace AirlineTicketsNotifications.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occured");
+                await HandleExceptionAsync(httpContext);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = context?.Features?.Get<IExceptionHandlerFeature>()?.Error?.Message
+            }.ToString());
+        }
+    }
+}
