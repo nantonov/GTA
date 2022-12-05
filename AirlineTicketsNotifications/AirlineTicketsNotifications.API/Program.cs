f76d2ebcbbeaa9b9ae99f6d64896dea446f3fff6
Gleb <gleb.berezkin.2017@gmail.com>
2022-12-05 15:00:19 +0300

    Add swagger authentication

--- AirlineTicketsNotifications/AirlineTicketsNotifications.API/Program.cs
+++ AirlineTicketsNotifications/AirlineTicketsNotifications.API/Program.cs
@@ -5,6 +5,11 @@ using AirlineTicketsNotifications.API.ViewModels.NotificationRequest;
 using AirlineTicketsNotifications.API.ViewModels.TicketInfo;
 using AirlineTicketsNotifications.BLL.DI;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,10 +17,75 @@ var builder = WebApplication.CreateBuilder(args);
 
 var configurationBuilder = builder.Configuration;
 
+builder.Services.AddCors(config =>
+{
+    config.AddPolicy("DefaultPolicy",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost/3000").AllowAnyMethod().AllowAnyHeader();
+        });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = configurationBuilder["Urls:Authority"];
+    options.RequireHttpsMetadata = false;
+    options.Audience = "AirlineTicketsNotificationsAPI";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorization();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Description = "Demo Swagger API v1",
+        Title = "Swagger with IdentityServer4",
+        Version = "1.0.0"
+    });
+
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Password = new OpenApiOAuthFlow
+            {
+                TokenUrl = new Uri(configurationBuilder["Urls:Token"]),
+                Scopes = new Dictionary<string, string>
+                {
+                    {"AirlineTicketsNotificationsAPI", "AirlineTicketsNotifications API"}
+                }
+            }
+        }
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddBLLLogicDependencies(configurationBuilder);
 
@@ -30,13 +100,23 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "AirlineTicketsNotifications API");
+        options.DocumentTitle = "Title";
+        options.RoutePrefix = "docs";
+        options.DocExpansion(DocExpansion.List);
+        options.OAuthClientId("client_id_notifications");
+        options.OAuthScopeSeparator(" ");
+        options.OAuthClientSecret("client_secret_notifications");
+    });
 }
 
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
