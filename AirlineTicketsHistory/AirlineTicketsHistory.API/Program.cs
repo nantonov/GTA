03b454beb20a5128f9b2d647d871c80f179b389a
Gleb <gleb.berezkin.2017@gmail.com>
2022-12-10 13:38:23 +0300

    Add swagger authentication

--- AirlineTicketsHistory/AirlineTicketsHistory.API/Program.cs
+++ AirlineTicketsHistory/AirlineTicketsHistory.API/Program.cs
@@ -4,6 +4,11 @@ using AirlineTicketsHistory.API.Validation.Validators;
 using AirlineTicketsHistory.API.ViewModels.AirlineTicket;
 using AirlineTicketsHistory.BLL.DI;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,10 +25,66 @@ builder.Services.AddCors(config =>
         });
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = configurationBuilder["Urls:Authority"];
+    options.RequireHttpsMetadata = false;
+    options.Audience = "AirlineTicketsHistoryAPI";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorization();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Description = "Demo Swagger API v1",
+        Title = "Swagger with IdentityServer4",
+        Version = "1.0.0"
+    });
+
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Password = new OpenApiOAuthFlow
+            {
+                TokenUrl = new Uri(configurationBuilder["Urls:Token"]),
+                Scopes = new Dictionary<string, string>
+                {
+                    {"AirlineTicketsHistoryAPI", "AirlineTicketsHistory API"}
+                }
+            }
+        }
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddBLLLogicDependencies(configurationBuilder);
 
@@ -37,12 +98,24 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "AirlineTicketsHistory API");
+        options.DocumentTitle = "Title";
+        options.RoutePrefix = "docs";
+        options.DocExpansion(DocExpansion.List);
+        options.OAuthClientId("client_id_history");
+        options.OAuthScopeSeparator(" ");
+        options.OAuthClientSecret("client_secret_history");
+    });
 }
 
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseCors("DefaultPolicy");
 
