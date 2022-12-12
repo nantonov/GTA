787adbab6c8577e03d2f497461e517d3f9c5a1d8
Gleb <gleb.berezkin.2017@gmail.com>
2022-12-12 15:22:31 +0300

    Add test for HttpClientService

---
+++ AirlineTickets/Tests/AirlineTickets.BLL.Tests/Tests/HttpClientServiceTests.cs
@@ -0,0 +1,24 @@
+﻿namespace AirlineTickets.BLL.Tests.Tests
+{
+    public class HttpClientServiceTests
+    {
+        private readonly Mock<IHttpClientFactory> _httpClientFactory;
+        private readonly IHttpClientService _httpClientService;
+
+        public HttpClientServiceTests()
+        {
+            _httpClientFactory = new Mock<IHttpClientFactory>();
+            _httpClientService = new HttpClientService(_httpClientFactory.Object);
+        }
+
+        [Fact]
+        public async Task GetAuthNotificationsClientAsync_WhenMethodIsInvoked_ShouldReturnClient()
+        {
+            _httpClientFactory.Setup(f => f.CreateClient(string.Empty)).Returns(new HttpClient());
+
+            var result = await _httpClientService.GetAuthNotificationsClientAsync(default);
+
+            result.ShouldBeAssignableTo(typeof(HttpClient));
+        }
+    }
+}
