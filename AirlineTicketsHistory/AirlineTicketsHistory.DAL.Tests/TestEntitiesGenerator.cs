f7fe2453d17fc14983d69f564c3295fc7bc0749c
Gleb <gleb.berezkin.2017@gmail.com>
2022-12-13 00:04:14 +0300

    Add DAL layer tests

---
+++ AirlineTicketsHistory/AirlineTicketsHistory.DAL.Tests/TestEntitiesGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace AirlineTicketsHistory.DAL.Tests
+{
+    public static class TestEntitiesGenerator
+    {
+        private static Fixture _fixtureBuilder = new();
+
+        public static UserTicketsHistoryEntity GetUserTicketsHistoryEntity() => _fixtureBuilder.Build<UserTicketsHistoryEntity>()
+            .Without(h => h.AirlineTickets).With(h => h.UserId, "user").Create();
+    }
+}
---
+++ AirlineTicketsHistory/AirlineTicketsHistory.DAL.Tests/Usings.cs
@@ -0,0 +1,7 @@
+global using AirlineTicketsHistory.DAL.Context;
+global using AirlineTicketsHistory.DAL.Entities;
+global using AirlineTicketsHistory.DAL.Interfaces;
+global using AirlineTicketsHistory.DAL.Repositories;
+global using AutoFixture;
+global using Shouldly;
+global using Xunit;
