5f21c6896f466fbd1e83b37a35d88476e1103f76
Gleb <gleb.berezkin.2017@gmail.com>
2022-12-13 09:52:18 +0300

    Add BLL layer tests

---
+++ AirlineTicketsHistory/AirlineTicketsHistory.BLL.Tests/TestObjects/Entities/UserTicketHistoryEntities.cs
@@ -0,0 +1,28 @@
+﻿namespace AirlineTicketsHistory.BLL.Tests.TestObjects.Entities
+{
+    internal class UserTicketHistoryEntities
+    {
+        public static UserTicketsHistoryEntity HistoryEntity = new()
+        {
+            UserId = "user"
+        };
+
+        public static List<UserTicketsHistoryEntity> HistoryEntitiesList = new()
+        {
+            new UserTicketsHistoryEntity()
+            {
+                UserId = "user1",
+            },
+
+            new UserTicketsHistoryEntity()
+            {
+                UserId = "user2",
+            },
+
+            new UserTicketsHistoryEntity()
+            {
+                UserId = "user3",
+            },
+        };
+    }
+}
---
+++ AirlineTicketsHistory/AirlineTicketsHistory.BLL.Tests/Usings.cs
@@ -0,0 +1,13 @@
+global using Xunit;
+global using AutoMapper;
+global using Moq;
+global using Shouldly;
+
+global using AirlineTicketsHistory.BLL.Interfaces;
+global using AirlineTicketsHistory.BLL.Models;
+global using AirlineTicketsHistory.BLL.Services;
+global using AirlineTicketsHistory.BLL.Tests.TestObjects.Entities;
+global using AirlineTicketsHistory.BLL.Tests.TestObjects.Models;
+global using AirlineTicketsHistory.DAL.Entities;
+global using AirlineTicketsHistory.DAL.Interfaces;
+global using AirlineTicketsHistory.DAL.Repositories;
