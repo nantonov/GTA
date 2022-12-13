cc8796a73912c00e1c911033f38054aaf2bdfb05
Gleb <81156189+Glipchik@users.noreply.github.com>
2022-12-13 12:31:23 +0300

    Delete empty line

--- AirlineTicketsHistory/AirlineTicketsHistory.API.Tests/Usings.cs
+++ AirlineTicketsHistory/AirlineTicketsHistory.API.Tests/Usings.cs
@@ -2,7 +2,6 @@ global using Xunit;
 global using Shouldly;
 global using System.Net;
 
-
 global using AirlineTicketsHistory.API.Tests.TestEntities;
 global using AirlineTicketsHistory.DAL.Entities;
 global using AirlineTicketsHistory.API.Tests.Constants;
